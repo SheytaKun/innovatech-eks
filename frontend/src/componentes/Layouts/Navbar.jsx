99698b6b401b57e7ee47535b0d6b7b2496137adc
Sebastian mariqueo <seb.mariqueo@duocuc.cl>
2026-07-08 20:25:22 -0400

    Se hace la prueba del pipeline para la demostracion del examen

--- frontend/src/componentes/Layouts/Navbar.jsx
+++ frontend/src/componentes/Layouts/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar() {
   return (
     <nav className="rounded-xl w-[250px] min-h-[880px] bg-fuchsia-900 text-white sticky top-0 p-4 m-4">
       {/* Logo o título */}
-      <h2 className="text-xl font-bold mb-8">Despacho Dashboard para la evaluacion tres Version 2</h2>
+      <h2 className="text-xl font-bold mb-8">Despacho Dashboard para el examen</h2>
 
       {/* Menú de navegación */}
       <ul className="space-y-3">
@@ -11,7 +11,7 @@ function Navbar() {
             href="#"
             className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
           >
-            Usuarios
+            Usuarios examen
           </a>
         </li>
         <li>
@@ -19,7 +19,7 @@ function Navbar() {
             href="#"
             className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
           >
-            Productos
+            Productos examen
           </a>
         </li>
         <li>
@@ -27,7 +27,7 @@ function Navbar() {
             href="#"
             className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
           >
-            Configuración
+            Configuración examen
           </a>
         </li>
       </ul>
